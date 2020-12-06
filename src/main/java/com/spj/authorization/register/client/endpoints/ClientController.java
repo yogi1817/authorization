52faaffef0d03f33cf92ae579154487d0271b752
Yogesh Sharma <yogesh4004@example.com>
2020-12-06 00:26:58 -0500

    Adding service change

--- src/main/java/com/spj/authorization/register/client/endpoints/ClientController.java
+++ src/main/java/com/spj/authorization/register/client/endpoints/ClientController.java
@@ -1,17 +1,17 @@
 package com.spj.authorization.register.client.endpoints;
 
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
 import com.spj.authorization.register.client.ports.in.IClientAdapter;
 import com.spj.register.openapi.endpoint.RegisterClientApi;
 import com.spj.register.openapi.resources.RegisterClientRequest;
 import com.spj.register.openapi.resources.RegisterClientResponse;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-import javax.validation.Valid;
 
 @Controller
 @Slf4j
